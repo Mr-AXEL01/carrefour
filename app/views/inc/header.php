1048bc8931a88e4427bface5462e44af6ca56e76
mouche <144794503+Mouche00@users.noreply.github.com>
2024-01-02 08:44:11 +0000

    add folder structure

---
+++ app/views/inc/header.php
@@ -0,0 +1,15 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <script src="https://kit.fontawesome.com/1a53866afa.js" crossorigin="anonymous"></script>
+    <script src="https://cdn.tailwindcss.com"></script>
+
+    <link rel="stylesheet" href="<?php echo URLROOT;?>/css/style.css">
+
+    <title><?php echo SITENAME; ?></title>
+</head>
+<body>
+    <?php require APPROOT . '/views/inc/navbar.php'; ?>
+    <div class="container"> <!-- Begin Main Container -->
