395bbc9a509c5634aae7e2b10833b64057a5da3e
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2024-01-03 17:29:19 +0100

    create method to display the orders

--- app/controllers/Order.php
+++ app/controllers/Order.php
@@ -8,4 +8,11 @@ class Order extends controller {
         $this->model = $this->model('Order');
         $this->service = $this->service('Order');
     }
+
+    public function display () {
+        $orders = $this->service->read();
+        echo json_encode($orders);
+    }
+
+    
 }
