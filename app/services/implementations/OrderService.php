16481731272cf846152635e37bc521474502ce36
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2024-01-03 16:27:00 +0100

    create method to update oders .

--- app/services/implementations/OrderService.php
+++ app/services/implementations/OrderService.php
@@ -24,4 +24,15 @@ class OrderService implements OrderServiceInterface {
         $this->db->query($sql);
         return $this->db->resultSet();
     }
+
+    public function update (Order $order) {
+        $idOrder = $order->getIdOrder();
+        $idClient = $order->getIdClient();
+        
+        $this->db->query("UPDATE order SET idClient = :idClient WHERE idOrder = :idOrder");
+        $this->db->bind(":idOrder", $idOrder);
+        $this->db->bind(":idClient", $idClient);
+
+        return $this->db->execute();
+    }
 }
