50f7c5e56205dec342da73229e5087c63d4db181
hafssahajou <hafssahajou200@gmail.com>
2024-01-03 14:43:43 +0100

    finish models cart

--- app/models/Cart.php
+++ app/models/Cart.php
@@ -1,2 +1,42 @@
 
 
+<?php
+
+class Cart
+{
+    private $idcart;
+
+
+
+    public function __construct()
+{
+
+
+}
+
+
+
+    public function getIdcart()
+    {
+        return $this->idcart;
+    }
+
+    /**
+     * Set the value of idcart
+     *
+     * @return  self
+     */ 
+    public function setIdcart($idcart)
+    {
+        $this->idcart = $idcart;
+
+        return $this;
+    }
+}
+
+
+
+
+
+
+?>
