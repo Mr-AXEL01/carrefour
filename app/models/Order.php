c35dcebc8c3194263f57971d13a014b0769b5843
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2024-01-03 12:30:30 +0100

    create modal for order

---
+++ app/models/Order.php
@@ -0,0 +1,27 @@
+<?php
+
+class Order {
+    private $idOrder;
+    private $idClient;
+
+    public function __construct($idOrder, $idClient) {
+        $this->idOrder  = $idOrder;
+        $this->idClient = $idClient;
+    }
+
+    #getter
+    public function getIdOrder() {
+        return $this->idOrder;
+    }
+    public function getIdClient() {
+        return $this->idClient;
+    }
+
+    #setter
+    public function setIdOrder($idOrder) {
+        $this->idOrder = $idOrder;
+    }
+    public function setIdClient($idClient) {
+        $this->idClient = $idClient;
+    }
+}
