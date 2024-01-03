8da8823e8b35c93cb13dca1e2198cf651413f94a
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2024-01-03 17:25:42 +0100

    create constructor for the order controller

--- app/controllers/Order.php
+++ app/controllers/Order.php
@@ -3,4 +3,9 @@
 class Order extends controller {
     private $model;
     private $service;
+
+    public function __construct() {
+        $this->model = $this->model('Order');
+        $this->service = $this->service('Order');
+    }
 }
