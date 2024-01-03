707f8e5659bccf09129611b90598d4c7f9eb4f32
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2024-01-03 15:27:53 +0100

    create methode for creating order

--- app/services/implementations/OrderService.php
+++ app/services/implementations/OrderService.php
@@ -6,4 +6,15 @@ class OrderService implements OrderServiceInterface {
     public function __construct() {
         $this->db = Database::getInstance();
     }
+
+    public function create (Order $order) {
+        $idOrder = $order->getIdOrder();
+        $idClient = $order->getIdClient();
+
+        $this->db->query("INSERT INTO order VALUES (:idOrder, :idClient)");
+        $this->db->bind(":idOrder", $idOrder);
+        $this->db->bind(":idClient", $idClient);
+
+        return $this->db->execute();
+    }
 }
