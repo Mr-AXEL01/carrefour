036586d3efeb970d34f96a2488b08e32d649320d
chaabat <chaabat.02@gmail.com>
2024-01-04 14:42:31 +0100

    admin ajax

--- app/config/config.php
+++ app/config/config.php
@@ -1,9 +1,6 @@
 <?php
 
-<<<<<<< HEAD
 //
-=======
->>>>>>> 36de1a8d431b2647930faa3ab7cbac75cabe428f
 $config = parse_ini_file('config.ini');
 
 // DB Params
@@ -18,12 +15,4 @@ define('APPROOT', dirname(dirname(__FILE__)));
 // URL Root
 define('URLROOT', 'http://localhost/carrefour');
 
-<<<<<<< HEAD
 define('SITENAME', 'Carrefour');
-=======
-// Site Name
-define('SITENAME', 'Carrefour');
-
-// App Version
-define('APPVERSION', '1.0.0');
->>>>>>> 36de1a8d431b2647930faa3ab7cbac75cabe428f
