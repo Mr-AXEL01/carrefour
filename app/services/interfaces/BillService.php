42b7f804abb707b3bdc872fdd6cd0f5aa9a0f592
chaabat <chaabat.02@gmail.com>
2024-01-04 09:57:20 +0100

    ajax user

--- app/services/interfaces/BillService.php
+++ app/services/interfaces/BillService.php
@@ -1,14 +1,10 @@
-
-
-
-
 <?php
 
 interface BillService
  {
 
     public function create(Bill $Bill);
-    public function read()
+    public function read();
     public function update(Bill $Bill);
     public function delete($id);
     public function fetch($id);
