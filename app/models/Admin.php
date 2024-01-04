e3e48ddd17487f8d9734688ead99de5198d15944
mouche <144794503+Mouche00@users.noreply.github.com>
2024-01-04 13:51:52 +0000

    fix routing

---
+++ app/models/Admin.php
@@ -0,0 +1,73 @@
+<?php
+
+class Admin extends User {
+
+    public function __construct ()
+    {
+    }
+
+    public function getIdUser(){
+		return $this->idUser;
+	}
+
+	public function setIdUser($idUser){
+		$this->idUser = $idUser;
+	}
+
+	public function getProfil(){
+		return $this->profil;
+	}
+
+	public function setProfil($profil){
+		$this->profil = $profil;
+	}
+
+	public function getFullNAme(){
+		return $this->fullNAme;
+	}
+
+	public function setFullNAme($fullNAme){
+		$this->fullNAme = $fullNAme;
+	}
+
+	public function getUsername(){
+		return $this->username;
+	}
+
+	public function setUsername($username){
+		$this->username = $username;
+	}
+
+	public function getEmail(){
+		return $this->email;
+	}
+
+	public function setEmail($email){
+		$this->email = $email;
+	}
+
+	public function getPassword(){
+		return $this->password;
+	}
+
+	public function setPassword($password){
+		$this->password = $password;
+	}
+
+	public function getPhone(){
+		return $this->phone;
+	}
+
+	public function setPhone($phone){
+		$this->phone = $phone;
+	}
+
+	public function getAdress(){
+		return $this->adress;
+	}
+
+	public function setAdress($adress){
+		$this->adress = $adress;
+	}
+
+}
